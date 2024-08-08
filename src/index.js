c8388748e14b007ebb2b4afd0092037765b2ba5d
여최유진 <yeochoeyujin@yeochoeyujin-ui-MacBookAir.local>
2024-08-08 22:27:45 +0900

    240808

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import authReducer from "./state";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import {persistStore,persistReducer,FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER,} from "redux-persist"; //현재 페이지가 유지되도록 해주는 것.
+import {persistStore,persistReducer,FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER,} from "redux-persist"; 
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 
